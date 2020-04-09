261fc165709beaf8acd72d479380fb69ad121e60
Eric Fu <u5885811@anu.edu.au>
2020-04-10 00:45:40 +1000

    Revert "before revert"
    This reverts commit 04b9729fb24e84ce3d1596a571764be3084e4ed2.
    
    revert done wrong, abort

--- src/util/GoogleMap.js
+++ src/util/GoogleMap.js
@@ -2,7 +2,11 @@ const apiKey = "AIzaSyDPmDFRL1kx6_1-yJ832fnXHcDzw4-ETxc";
 
 const GMap = {
     autoComplete(term) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${term}&types=geocode&key=${apiKey}`).then(
+        return fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${term}&types=geocode&language=en&key=${apiKey}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            },
+        }).then(
             response => {
                 return response.json();
             }
