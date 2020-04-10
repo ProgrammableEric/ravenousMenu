b9c1f91f5afee78970723b2ace4af7d2a3f09d13
Eric Fu <u5885811@anu.edu.au>
2020-04-10 22:41:59 +1000

    completed autocomplete functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './SearchBar.css'
 import GMap from '../../util/GoogleMap'
 import pin from '../../assets/pin.png'
-
+import gps from '../../assets/gps.png'
+import aim from '../../assets/aim.png'
 
 
 class SearchBar extends React.Component {
@@ -13,7 +14,8 @@ class SearchBar extends React.Component {
             term: '',
             location: '',
             sortBy: 'best_match',
-            predictions: []
+            predictions: [], 
+            show: false
         };
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
@@ -24,6 +26,7 @@ class SearchBar extends React.Component {
         }
         this.handleSearch = this.handleSearch.bind(this);
         this.searchGMap = this.searchGMap.bind(this);
+        this.handlePredictionSearch = this.handlePredictionSearch.bind(this);
     }
 
     searchGMap(term) {
@@ -57,6 +60,13 @@ class SearchBar extends React.Component {
     handleLocationChange(event) {
         this.setState({ location: event.target.value});
         this.searchGMap( event.target.value );
+        this.state.show = true;
+        if (!event.target.value) this.state.show = false;
+    }
+
+    handlePredictionSearch(description) {
+        this.setState({ location: description});
+        this.setState({ show: false});
     }
 
     renderSortByOptions() {
@@ -70,22 +80,52 @@ class SearchBar extends React.Component {
             </li>)
         });
     }
-    
 
+    renderAutoComplete() {
+        const predictions = this.state.predictions;
+        return (predictions.map( (prediction) => {
+            const id = prediction.id;
+            const description = prediction.description;
+            return (
+                <li key={id}>
+                    <button className="Prediction-submit" onClick={() => this.handlePredictionSearch(description)}>
+                        <div className="buttonFill">
+                            <img src={aim} alt='little' className="aimIcon"/>
+                            <div className="text">{description}</div>
+                        </div>
+                    </button>
+                </li>
+            )
+        })
+        ); 
+    }
+    
     render() {
         return (
             <div className="SearchBar" >
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions()}
-                    </ul>
+                        {this.renderSortByOptions}
+                    </ul> 
                 </div>
                 <div className="SearchBar-fields"> 
                     <input className="Term" placeholder="Search Businesses" onChange={this.handleTermChange} />
                     <div className="LocationWrapper">
                         <img src={pin} alt="pin icon" className="pin"/>
-                        <input placeholder="Where?" onChange={this.handleLocationChange}/>
+                        <input placeholder="Where?" onChange={this.handleLocationChange} value={this.state.location}/>
                     </div>
+                    
+                    {   this.state.show ? (
+                        <div className="autoCompleteWrapper">
+                            <ul className="autoCompleteList">
+                                {this.renderAutoComplete()}
+                            </ul>
+                            <div className="poweredBy">
+                                <span>Powered by</span>
+                                <img className="poweredByIcon" src={gps} alt="google logo"/>
+                            </div> 
+                        </div>   
+                    ) : null}
                 </div>
                 <div className="SearchBar-submit">
                     <a onClick={this.handleSearch}>Let's Go</a>
--- src/util/GoogleMap.js
+++ src/util/GoogleMap.js
@@ -2,7 +2,7 @@ const apiKey = "AIzaSyDPmDFRL1kx6_1-yJ832fnXHcDzw4-ETxc";
 
 const GMap = {
     autoComplete(term) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${term}&types=geocode&key=${apiKey}`).then(
+        return fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${term}&types=geocode&language=en&key=${apiKey}`).then(
             response => {
                 return response.json();
             }
@@ -11,6 +11,7 @@ const GMap = {
                 if (jsonResponse.predictions){
                     console.log(jsonResponse.status);
                     return jsonResponse.predictions.map( (prediction) => {
+                        console.log(prediction.id, ':  ', prediction.description);
                         return {
                             id: prediction.id,
                             description: prediction.description 
